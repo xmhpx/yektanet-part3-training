8e3b16fa9bb4fc4391e38f165adc7e193124284e
xmhpx <mhppaydar@gmail.com>
2021-10-04 23:14:23 +0330

    Added main.py still doesn't work, todo: fix the bugs

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
 import base_model
 import advertiser
+import ad
+
+print("ss")
+
+baseAdvertising = base_model.BaseAdvertising()
+advertiser1 = advertiser.Advertiser(1, 'name1')
+advertiser2 = advertiser.Advertiser(2, 'name2')
+
+ad1 = ad.Ad(1, 'title1', 'img-url1', 'link1', 'advertiser1')
+ad2 = ad.Ad(2, 'title2', 'img-url2', 'link2', 'advertiser2')
+
+baseAdvertising.describeMe()
+ad2.describeMe()
+advertiser1.describeMe()
+ad1.incViews()
+ad1.incViews()
+ad1.incViews()
+ad1.incViews()
+ad2.incViews()
+ad1.incClicks()
+ad1.incClicks()
+ad2.incClicks()
+advertiser2.getName()
+advertiser2.setName('new name')
+advertiser2.getName()
+ad1.getClicks()
+advertiser2.getClicks()
+advertiser.Advertiser.getTotalClicks()
+advertiser.Advertiser.help()
