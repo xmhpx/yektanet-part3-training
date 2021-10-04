3bc57d99ba96396c07a85c490b96771f401a9e4d
xmhpx <mhppaydar@gmail.com>
2021-10-04 22:47:51 +0330

    Added advertiser.py

---
+++ advertiser.py
@@ -0,0 +1,31 @@
+import base_model
+
+
+class Advertiser(base_model.BaseAdvertising):
+    __totalClicks = 0
+
+    def __int__(self, id, name):
+        self.__id = id
+        self.__name = name
+
+    def getName(self):
+        return self.__name
+
+    def setName(self, name):
+        self.__name = name
+
+    @staticmethod
+    def help(self):
+        print("I\'m helping")
+
+    @staticmethod
+    def describeMe(self):
+        print("I\'m the Advertiser class")
+
+    @staticmethod
+    def getTotalClicks(self):
+        return Advertiser.__totalClicks
+
+    def incClicks(self):
+        super(Advertiser, self).incClicks()
+        Advertiser.__totalClicks += 1
