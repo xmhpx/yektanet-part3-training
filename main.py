f9518b659052c5035d2e66a3b02e715e8eeb7dd3
xmhpx <mhppaydar@gmail.com>
2021-10-05 13:32:51 +0330

    Fixed a typo in main ('advertiser1' -> advertiser)

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from base_model import BaseAdvertising
 from advertiser import Advertiser
 from ad import Ad
 
-baseAdvertising = BaseAdvertising()
+baseAdvertising = BaseAdvertising(5)
 advertiser1 = Advertiser(1, 'name1')
 advertiser2 = Advertiser(2, 'name2')
 
-ad1 = Ad(1, 'title1', 'img-url1', 'link1', 'advertiser1')
-ad2 = Ad(2, 'title2', 'img-url2', 'link2', 'advertiser2')
+ad1 = Ad(1, 'title1', 'img-url1', 'link1', advertiser1)
+ad2 = Ad(2, 'title2', 'img-url2', 'link2', advertiser2)
 
 baseAdvertising.describeMe()
 ad2.describeMe()
@@ -20,10 +20,10 @@ ad2.incViews()
 ad1.incClicks()
 ad1.incClicks()
 ad2.incClicks()
-advertiser2.getName()
+print(advertiser2.getName())
 advertiser2.setName('new name')
-advertiser2.getName()
-ad1.getClicks()
-advertiser2.getClicks()
-Advertiser.getTotalClicks()
+print(advertiser2.getName())
+print(ad1.getClicks())
+print(advertiser2.getClicks())
+print(Advertiser.getTotalClicks())
 Advertiser.help()
